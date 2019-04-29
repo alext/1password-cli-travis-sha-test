e272e9bd096cf7e575e689448bebeadc8e29e93a
Alex Tomlins <alex@tomlins.org.uk>
2019-04-29 10:35:30 +0100

    Test shasum of op-cli...

--- test.sh
+++ test.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-exit 0
+set -eux
+
+wget -O op_cli.zip https://cache.agilebits.com/dist/1P/op/pkg/v0.5.5/op_darwin_amd64_v0.5.5.zip
+
+expected=e4ea329debcf991434d90728fa3cba531bce5449a08883d3530dfeb796fc3a3b
+actual=$(shasum -a 256 "./op_cli.zip" | awk '{print $1}')
+
+if [ "${actual}" = "${expected}" ]; then
+  echo "shasum matches"
+  echo "Downloaded file has checksum: ${actual}"
+else
+  echo -e "shasum mismatch.\nwant: ${expected}\ngot:  ${actual}"
+  exit 1
+fi
