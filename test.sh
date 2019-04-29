0e9c70558eccfa9cf5f5549564a3ff7ec87694b5
Alex Tomlins <alex@tomlins.org.uk>
2019-04-29 10:58:43 +0100

    Moar testing

--- test.sh
+++ test.sh
@@ -4,6 +4,11 @@ set -eux
 
 wget -O op_cli.zip https://cache.agilebits.com/dist/1P/op/pkg/v0.5.5/op_darwin_amd64_v0.5.5.zip
 
+ls -l op_cli.zip
+shasum -a 256 -t op_cli.zip
+shasum -a 256 -b op_cli.zip
+shasum -a 256 -0 op_cli.zip
+
 expected=e4ea329debcf991434d90728fa3cba531bce5449a08883d3530dfeb796fc3a3b
 actual=$(shasum -a 256 "./op_cli.zip" | awk '{print $1}')
 
